9cfb9980143e4816d852d5a117dc909166be4a6a
armando <jarodriguezvera@gmail.com>
2024-04-01 12:34:12 -0600

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace UsuarioLibrary
+{
+    public class Usuario
+    {
+        #region Atributos
+        private string nombre;
+        private string password;
+        #endregion
+
+        #region Propiedades
+        public string Nombre {
+            get => nombre;
+            set
+            {
+                if( value == "")
+                {
+                    nombre = "Anonymous";
+                }
+                else
+                {
+                    nombre = value;
+
+                }
+            }
+ 
+        }
+        public string Password {
+            get => password;
+            set
+            {
+                if( value == "")
+                {
+                    password = GenerateRandPassword(15);
+                }
+                else
+                {
+                    password = value;
+                }
+
+                
+            } 
+        }
+        #endregion
+
+        #region Métodos
+
+        private string GenerateRandPassword(int longPass)
+        {
+            Random random = new Random();
+            string password = "";
+
+            for(int i=0; i< longPass; i++)
+            {
+                password += (char)random.Next(255);
+
+
+            }
+            return password;
+
+        }
+
+        #endregion
+
+        #region Constructores
+        public Usuario(string nombre, string password)
+        {
+            Nombre = nombre;
+            Password = password;
+        }
+        #endregion
+    }
+}
